2f650eb64b5e4bd6fe36ae862be1a5162ce00cda
Abdullah <aa3541900@gmail.com>
2026-09-22 00:42:07 +0530

    Two Pointers practice

--- 15_patterns/1-Two-Pointers/practiceQuestions/4-moving-zeros.cpp
+++ 15_patterns/1-Two-Pointers/practiceQuestions/4-moving-zeros.cpp
@@ -2,13 +2,42 @@
 #include <vector>
 using namespace std;
 
-void moveZeroes(vector<int>& nums){
-    int left = 0;
-    int right = nums.size() - 1;
 
-    while(left < right){
-        if(nums[left] == 0){
-            
+// Goal - Push all zeroes to the end. Keep everything else in order
+
+void moveZeroes(vector<int>& nums){
+    int slow = 0;
+
+    // Job 1: Scan with fast, place keeper using slow
+    for(int fast = 0; fast < nums.size(); fast++){
+        if(nums[fast] != 0){
+            nums[slow] = nums[fast];
+            slow++;
         }
     }
+
+    // Job 2: Everything from slow onwards becomes 0;
+    for(int i = slow; i < nums.size(); i++){
+        nums[i] = 0;
+    }
+}
+
+
+int main(){
+
+    vector<int> nums = {1, 0, 2, 0, 3, 0};
+
+    for(int x: nums){
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    moveZeroes(nums);
+
+    for(int x: nums){
+        cout << x << " ";
+    }
+
+    return 0;
 }
